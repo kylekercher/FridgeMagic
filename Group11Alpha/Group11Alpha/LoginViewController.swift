27c9d7d11a1c504a835c6ef3d6d788bad5d8c85b
kylekercher <kylekerch@yahoo.com>
2018-04-10 15:06:06 -0500

    Add files via upload

---
+++ Group11Alpha/Group11Alpha/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Group11Alpha
+//
+//  Created by Yeeun Choi on 2018. 3. 21..
+//  Copyright © 2018년 Yeeun Choi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func loginBtn(_ sender: Any) {
+    }
+    
+    @IBAction func facebookBtn(_ sender: Any) {
+    }
+    
+    @IBAction func googleBtn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func forgotBtn(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
